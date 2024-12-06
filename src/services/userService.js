427199d85608f282dc1d7cae1a8d04dfd2f70fde
camdelay16 <camdelay16@gmail.com>
2024-12-06 16:14:24 -0500

    made changes

--- src/services/userService.js
+++ src/services/userService.js
@@ -4,78 +4,85 @@ const BASE_URL = `${BACKEND_URL}/users`;
 
 const signup = async (formData) => {
   try {
-    const res = await axios.post(`${BASE_URL}/signup`, formData)
+    const res = await axios.post(`${BASE_URL}/signup`, formData);
     if (res.data.error) {
-        throw new Error(res.data.error)
-      }
+      throw new Error(res.data.error);
+    }
 
     if (res.data.token) {
-      localStorage.setItem('token', res.data.token)
-      const user = JSON.parse(atob(res.data.token.split('.')[1]));
-      return user
+      localStorage.setItem("token", res.data.token);
+      const user = JSON.parse(atob(res.data.token.split(".")[1]));
+      return user;
     }
-    return res.data
+    return res.data;
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
-}
+};
 
 const signin = async (user) => {
-    try {
-      const res = await axios.post(`${BASE_URL}/signin`, user)
-      if (res.data.error) {
-        throw new Error(res.data.error)
-      }
-  
-      if (res.data.token) {
-        localStorage.setItem('token', res.data.token)
-        const user = JSON.parse(atob(res.data.token.split('.')[1]));
-        return user
-      }
-    } catch (error) {
-      console.log(error)
+  try {
+    console.log(`${BASE_URL}/signin`);
+    const res = await axios.post(`${BASE_URL}/signin`, user);
+    if (res.data.error) {
+      throw new Error(res.data.error);
     }
-  }
 
-  const getUser = () =>  {
-    const token = localStorage.getItem('token');
-    if (!token) return null;
-    const user = JSON.parse(atob(token.split('.')[1]))
-    return user;
-  }
-
-  const getUserData = async (userId) => {
-    try {
-      const res = await axios.get(`${BASE_URL}/${userId}`, {headers: {Authorization: `Bearer ${localStorage.getItem('token')}`}})
-      if (!res) {
-        throw new Error(res.data.error)
-      }
-      return res.data
-    } catch (error) {
-      console.log(error)
+    if (res.data.token) {
+      localStorage.setItem("token", res.data.token);
+      const user = JSON.parse(atob(res.data.token.split(".")[1]));
+      return user;
     }
+  } catch (error) {
+    console.log(error);
   }
-  
-  const signout = () => {
-    localStorage.removeItem('token')
+};
+
+const getUser = () => {
+  const token = localStorage.getItem("token");
+  if (!token) return null;
+  const user = JSON.parse(atob(token.split(".")[1]));
+  return user;
+};
+
+const getUserData = async (userId) => {
+  try {
+    const res = await axios.get(`${BASE_URL}/${userId}`, {
+      headers: { Authorization: `Bearer ${localStorage.getItem("token")}` },
+    });
+    if (!res) {
+      throw new Error(res.data.error);
+    }
+    return res.data;
+  } catch (error) {
+    console.log(error);
   }
+};
+
+const signout = () => {
+  localStorage.removeItem("token");
+};
 
 const update = async (userId, formData) => {
   try {
-    const res = await axios.put(`${BASE_URL}/${userId}`, formData, {headers: {Authorization: `Bearer ${localStorage.getItem('token')}`}})
+    const res = await axios.put(`${BASE_URL}/${userId}`, formData, {
+      headers: { Authorization: `Bearer ${localStorage.getItem("token")}` },
+    });
     return res.data;
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
-}
+};
 
 const deleteUser = async (userId) => {
   try {
-    const res = await axios.delete(`${BASE_URL}/${userId}`, {headers: {Authorization: `Bearer ${localStorage.getItem('token')}`}})
+    const res = await axios.delete(`${BASE_URL}/${userId}`, {
+      headers: { Authorization: `Bearer ${localStorage.getItem("token")}` },
+    });
     return res.data;
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
-}
+};
 
-export { signup, signin, signout, getUser, getUserData, update, deleteUser }
+export { signup, signin, signout, getUser, getUserData, update, deleteUser };
